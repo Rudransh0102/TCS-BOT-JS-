e27df567da2e097bd3320b18b9259f23ebd619d5
Rudransh_0110 <110889201+Rudransh0102@users.noreply.github.com>
2023-03-02 23:31:53 +0530

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,29 @@
+const discord = require("discord.js");
+const client = new discord.Client();
+const fetch = require("node-fetch");
+require("dotenv").config();
+
+client.on("ready", () => {
+  console.log(`${client.user.tag} is up and ready!`);
+});
+
+client.on("message", async (msg) => {
+  try{
+  let query = msg.content.split(" ");
+  if (query[0] == ".gif") {
+    let url = `https://g.tenor.com/v1/search?q=${query[1]}&key=${process.env.TENOR}`;
+    let response = await fetch(url);
+    let data = await response.json();
+    let random = Math.floor(Math.random() * data.results.length); 
+    msg.channel.send(data.results[random].url);
+  }}
+  catch(err){
+      console.log(err);
+  }
+});
+
+try {
+  client.login("MTA2MzA4NTgwMTE2NjA5ODQ0Mg.G9YKgT.C8kov5b0UszQiy700vUvt2jbqBZDOTQdkAlB5o");
+} catch (error) {
+  console.log(`There is some error in token ${error}`);
+}
